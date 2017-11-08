33c049b0e91ae6141847a88fcdd8b464903a8654
Stefan Pająk <stefpaj@o2.pl>
2017-11-08 20:37:36 +0100

    Pierwsze wczytanie i zapisanie obrazu

--- POMwJO/POMwJO/MainWindow.xaml.cs
+++ POMwJO/POMwJO/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using itk.simple;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,55 @@ namespace POMwJO
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] args = new string[3];
+            string s = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            args[0] = s+"Obrazy\\abc.png";
+            args[1] = "1";
+            args[2] = s+ "Obrazy\\cba.png";
+
+            try
+            {
+                if (args.Length < 3)
+                {
+                    //Console.WriteLine("Usage: SimpleGausian <imput> <sigma> <output>");
+                    lTest.Content = "Usage: SimpleGausian <imput> <sigma> <output>";
+                    return;
+                }
+                //Read imput image
+                ImageFileReader reader = new ImageFileReader();
+                reader.SetFileName(args[0]);
+                itk.simple.Image image = reader.Execute();
+
+                //Remember format
+                //itk.simple.Image image2 = new itk.simple.Image(image);
+
+                //PixelIDValueEnum id = PixelIDValueEnum.sitkVectorUInt8;
+                //PixelIDValueEnum id = image2.GetPixelID();
+
+                //Execute Gausian smoothing filter
+                //SmoothingRecursiveGaussianImageFilter gausian = new SmoothingRecursiveGaussianImageFilter();
+                //gausian.SetSigma(Double.Parse(args[1]));
+                //image = gausian.Execute(image);
+
+                //Convert?
+                //PixelIDValueEnum id = image.GetPixelID();
+                //SimpleITK.Cast(image, id);
+                //id = image.GetPixelID();
+
+                //Write output image
+                ImageFileWriter writer = new ImageFileWriter();
+                writer.SetFileName(args[2]);
+                writer.Execute(image);
+
+                lTest.Content = "Chyba się udało, sprwadź :P";
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex);
+                lTest.Content = ex;
+            }
         }
     }
 }
