56603f0b92a3ff09e261b2079f892739110c2cbd
Admin <Admin@ELRENN>
2017-11-09 13:36:47 +0100

    Wczytywanie z wyborem pliku

--- POMwJO/POMwJO/MainWindow.xaml.cs
+++ POMwJO/POMwJO/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using itk.simple;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,17 +26,70 @@ namespace POMwJO
         public MainWindow()
         {
             InitializeComponent();
+        }
+//=========================================================================================
+        /// <summary>
+        /// Wybranie ścieżki do wczytania pliku i wykonanie funkcji testowej(tymczasowe)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bWczytaj_Click(object sender, RoutedEventArgs e)
+        {
+            //Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
-            string[] args = new string[3];
-            string s = System.AppDomain.CurrentDomain.BaseDirectory;
+            string wanted_path = System.IO.Directory.GetCurrentDirectory();
+            //wanted_path = System.IO.Directory.GetCurrentDirectory();
             for (int i = 0; i < 5; i++)
             {
-                s = s.Remove(s.LastIndexOf("\\", s.Length - 2) + 1); 
+                wanted_path = System.IO.Path.GetDirectoryName(wanted_path);
             }
+            wanted_path += "\\Obrazy";
+            openFileDialog1.InitialDirectory = wanted_path;
+            openFileDialog1.Filter = "png files (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
 
-            args[0] = s+"Obrazy\\abc.png";
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                try
+                {
+                    TestowyOdczytIZapis(openFileDialog1.FileName);
+                    //if ((myStream = openFileDialog1.OpenFile()) != null)
+                    //{
+                    //    using (myStream)
+                    //    {
+                    //        // Insert code to read the stream here.
+                    //    }
+                    //}
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+//=========================================================================================
+        /// <summary>
+        /// Wczytanie obrazu z zadanie jścieżki i zapisanie go obok pod inną nazwą(tymczasowe)
+        /// </summary>
+        /// <param name="patch">Ścieżka do pliku</param>
+        private void TestowyOdczytIZapis(string patch)
+        {
+
+            string[] args = new string[3];
+            //string s = System.AppDomain.CurrentDomain.BaseDirectory;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    s = s.Remove(s.LastIndexOf("\\", s.Length - 2) + 1);
+            //}
+            //args[0] = s + "Obrazy\\abc.png";
+            //args[1] = "1";
+            //args[2] = s + "Obrazy\\cba.png";
+
+            args[0] = patch;
             args[1] = "1";
-            args[2] = s+ "Obrazy\\cba.png";
+            args[2] = patch.Insert(patch.LastIndexOf(".png"),"-kopia");
 
             try
             {
@@ -78,5 +133,6 @@ namespace POMwJO
                 lTest.Content = ex;
             }
         }
+//=========================================================================================
     }
 }
